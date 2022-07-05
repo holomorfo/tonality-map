55420daafb681b1043616b4dd69f1e643f0173c7
holomorfo <holomorfo.mx@gmail.com>
2022-07-04 21:21:30 -0700

    Initial logs

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { libraryInit, closeLibrary } from "./logs";
---
+++ src/logs.ts
@@ -0,0 +1,6 @@
+export function libraryInit() {
+  console.log("Initiate library");
+}
+export function closeLibrary() {
+  console.log("Close Library");
+}
