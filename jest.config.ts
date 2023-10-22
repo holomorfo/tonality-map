a8fba3beacca268f753fe3d5c572e9680122e141
Dr. Holomorfo <holomorfo.mx@gmail.com>
2023-10-21 19:00:10 -0700

    Update jest config to ignore dist

--- jest.config.ts
+++ jest.config.ts
@@ -4,9 +4,9 @@
  */
 
 export default {
+  modulePathIgnorePatterns: ["dist"],
   // All imported modules in your tests should be mocked automatically
   // automock: false,
-
   // Stop running tests after `n` failures
   // bail: 0,
 
